6cf6914c6173cf4c713f2c480515d3b1642f9505
Garcha <rishabhg@amazon.com>
2016-12-24 07:31:40 -0800

    Docker Integration

--- src/main/java/com/rish889/ECheckMobileRestApiApplication.java
+++ src/main/java/com/rish889/ECheckMobileRestApiApplication.java
@@ -2,11 +2,19 @@ package com.rish889;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ECheckMobileRestApiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ECheckMobileRestApiApplication.class, args);
+    }
+
+    @RequestMapping("/")
+    public String getRaces() {
+        return "Rishabh";
     }
 }
