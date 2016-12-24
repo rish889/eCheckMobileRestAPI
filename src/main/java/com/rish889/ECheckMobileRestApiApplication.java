3fdea4c230166416d4167c59b80375c8d1e27648
Garcha <rishabhg@amazon.com>
2016-12-24 06:38:48 -0800

    first commit

---
+++ src/main/java/com/rish889/ECheckMobileRestApiApplication.java
@@ -0,0 +1,12 @@
+package com.rish889;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECheckMobileRestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECheckMobileRestApiApplication.class, args);
+    }
+}
