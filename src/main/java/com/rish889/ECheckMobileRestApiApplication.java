f2b4b07a47ee3f1b29dcbe5069902cc757ac45b0
Garcha <rishabhg@amazon.com>
2016-12-24 12:28:43 -0800

    MySql Integration

--- src/main/java/com/rish889/ECheckMobileRestApiApplication.java
+++ src/main/java/com/rish889/ECheckMobileRestApiApplication.java
@@ -1,20 +1,61 @@
 package com.rish889;
 
+import hello.Customer;
+import hello.CustomerRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@RestController
+@EnableJpaRepositories("hello")
+@EntityScan("hello")
 public class ECheckMobileRestApiApplication {
 
+    private static final Logger log = LoggerFactory.getLogger(ECheckMobileRestApiApplication.class);
+
     public static void main(String[] args) {
-        SpringApplication.run(ECheckMobileRestApiApplication.class, args);
+        SpringApplication.run(ECheckMobileRestApiApplication.class);
     }
 
-    @RequestMapping("/")
-    public String getRaces() {
-        return "Rishabh";
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            // save a couple of customers
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
+
+            // fetch all customers
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
+
+            // fetch an individual customer by ID
+            Customer customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
+
+            // fetch customers by last name
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            for (Customer bauer : repository.findByLastName("Bauer")) {
+                log.info(bauer.toString());
+            }
+            log.info("");
+        };
     }
+
 }
